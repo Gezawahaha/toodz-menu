29cced74108aaacf0c1444571814d7f018eeaead
gezawahaha <geza.mahardika@gmail.com>
2022-08-26 23:26:35 +0700

    update dummy menu
    now dummy menu using monggodb conection

--- api/routes/dummymenu.js
+++ api/routes/dummymenu.js
@@ -1,16 +1,36 @@
 const router = require("express").Router();
+const Menu = require("../models/Menu");
 
 //GET ALL
-router.get("/", (req,res)=>{
+// router.get("/", (req,res)=>{
+//     const categorie = req.query.categorie;
+    
+//     let menu;
+//         try {
+//             menu = [
+//                 {"nama": "geza" },
+//                 {"nama": "adella" }
+//             ]
+            
+//             res.status(200).json(menu);
+//             // const menu = query ? await Menu.find().limit(10) : await Menu.find();
+//             // res.status(200).json(menu);
+            
+//         } catch (err) {
+//             res.status(500).json(err)
+//         }
+// });
+
+
+
+//GET ALL
+router.get("/", async (req,res)=>{
     const categorie = req.query.categorie;
     
     let menu;
         try {
-            menu = [
-                {"nama": "geza" },
-                {"nama": "adella" }
-            ]
-            
+            menu = await Menu.find();
+            console.log("Masuk Menu: ", menu);
             res.status(200).json(menu);
             // const menu = query ? await Menu.find().limit(10) : await Menu.find();
             // res.status(200).json(menu);
