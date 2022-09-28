774a97852e83e8bb8d3aa666073da186d83cf901
Gezawahaha <geza.mahardika@gmail.com>
2022-09-28 16:05:10 +0700

    Fixing API bugs

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ const App = () => {
                 const res = await axios.get("api/Toodzhouse/menu");
                 dispatch(setMenu({menu: res.data.data}));
             } catch (err) {
+                dispatch(setMenu({menu: { title: "Sate Padang", price: 20000, food_id: 1, categories: "Food", img: "https://img.inews.co.id/media/822/files/inews_new/2021/05/18/mencicipi_makanan_khas_indonesia.jpg" }}));
                 console.log(err);
             }
            
